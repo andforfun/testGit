cf7d6d3cc439e4fc669c87faaeac4ed235cb4113
Andy <andyboy315@gmail.com>
2017-04-18 16:31:35 +0800

    Test1

--- TestGit/ViewController.swift
+++ TestGit/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("123456")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
